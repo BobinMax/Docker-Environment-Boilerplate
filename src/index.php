74a822e2ad05932fbcaa48618b7405551afc0d95
Maxim Grigoriev <bobinmax@gmail.com>
2017-05-18 20:20:27 +0300

    Add Predis to composer dependency and modify index.php for example

--- src/index.php
+++ src/index.php
@@ -1,14 +1,28 @@
 <?php
+    // All of this code just for example
+    require __DIR__ . '/vendor/autoload.php';
+
+    use Predis\Client;
+
     $dsn = 'mysql:dbname=test;host=db';
     $user = 'root';
     $password = 'secret';
 
     try {
-        $pdo = new PDO($dsn, $user, $password);
+        $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     } catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
     }
 
-    $stat = $pdo->query('SELECT * FROM users');
-    $data = $stat->fetchAll();
-    var_dump($data);
+    try {
+        $stat = $pdo->query('SHOW DATABASES');
+        $data = $stat->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($data);
+    } catch (PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+
+    $client = new Predis\Client('tcp://cache:6379');
+    var_dump($client->set('foo', 'bar'));
+    $value = $client->get('foo');
+    var_dump($value);
