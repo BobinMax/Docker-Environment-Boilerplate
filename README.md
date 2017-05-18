9309fcbda4199a0f0b8bd7d6b6437ce2cd4669a3
Maxim Grigoriev <bobinmax@gmail.com>
2017-05-18 19:52:02 +0300

    Added mysql.cnf, added named volumes for storage data, added xdebug config, added composer files (for example), edited README.md

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
   - php7.1
   - mysql
   - redis
+
+### Pre requirements
+  - docker version 17.03.1-ce, and docker-compose version 1.11.2
+
+### Build process
+  - docker-compose up -d
+
+### Composer
+#### For Windows
+  - docker run --rm -v %cd%/src:/app composer/composer install
+#### For Linux
+  - docker run --rm -v $(pwd)/src:/app composer/composer install
--- src/index.php
+++ src/index.php
@@ -1,2 +1,14 @@
 <?php
-    phpinfo();
+    $dsn = 'mysql:dbname=test;host=db';
+    $user = 'root';
+    $password = 'secret';
+
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+
+    $stat = $pdo->query('SELECT * FROM users');
+    $data = $stat->fetchAll();
+    var_dump($data);
